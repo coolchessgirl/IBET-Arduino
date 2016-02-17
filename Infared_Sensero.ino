4d7cc904ea6b4a76d5744bae1356bef8d00728d5
Rachel Naidich <rachelnaidich1@gmail.com>
2016-02-16 21:56:17 -0500

    Create Infared_Sensero.ino

---
+++ Infared_Sensero.ino
@@ -0,0 +1,23 @@
+int light = 8;
+int sense = A0;
+int timetaken = 0;
+void setup()
+{
+  pinMode(light,OUTPUT);
+  pinMode(sense,INPUT);
+  digitalWrite(light,HIGH);
+  Serial.begin(9600);
+}
+void loop()
+{
+  int val=analogRead(sense);
+  Serial.println(val);
+  if(val>=900)
+  {
+    Serial.println("Something obstructed the sensor!");
+    Serial.println(timetaken);
+  }
+  delay(1000);
+  timetaken +=1;
+  
+}
